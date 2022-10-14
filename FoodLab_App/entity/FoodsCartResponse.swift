c4590911e04574b36ac4b4f5a1d5d5fdac9f424b
hexaque <37326156+hexaque@users.noreply.github.com>
2022-10-14 16:40:19 +0300

    que
    entity added

---
+++ FoodLab_App/entity/FoodsCartResponse.swift
@@ -0,0 +1,12 @@
+//
+//  FoodsCartResponse.swift
+//  FoodLab_App
+//
+//  Created by Que on 14.10.2022.
+//
+
+import Foundation
+class FoodsCartResponse : Codable{
+    var sepet_yemekler: [FoodsCart]?
+    var success:Int?
+}
---
+++ FoodLab_App/entity/FoodsResponse.swift
@@ -0,0 +1,12 @@
+//
+//  FoodsResponse.swift
+//  FoodLab_App
+//
+//  Created by Que on 14.10.2022.
+//
+
+import Foundation
+class FoodsResponse : Codable{
+    var yemekler: [Foods]?
+    var success:Int?
+}
