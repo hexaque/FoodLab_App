90496b5b4e3f44af76eea1b80d2d187e0e7b2af3
hexaque <37326156+hexaque@users.noreply.github.com>
2022-10-14 15:08:41 +0300

    Initial Commit

---
+++ FoodLab_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodLab_App
+//
+//  Created by Que on 14.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
