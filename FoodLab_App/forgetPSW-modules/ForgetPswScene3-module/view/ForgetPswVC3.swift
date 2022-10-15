2f4faa15e30396a0f79cf9002f4a12b5a7ff5e74
hexaque <37326156+hexaque@users.noreply.github.com>
2022-10-15 16:46:30 +0300

    que
    verifyCode eklendi

--- FoodLab_App/forgetPSW-modules/ForgetPswScene3-module/view/ForgetPswVC3.swift
+++ FoodLab_App/forgetPSW-modules/ForgetPswScene3-module/view/ForgetPswVC3.swift
@@ -7,23 +7,55 @@
 
 import UIKit
 
-class ForgetPswVC3: UIViewController {
+class ForgetPswVC3: UIViewController , UITextFieldDelegate {
 
+    @IBOutlet weak var labelError: UILabel!
     @IBOutlet weak var tbCode: UITextField!
+    let verifyCode = "9900"
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         navigationItem.hidesBackButton = true
+        labelError.isHidden = true
         // Do any additional setup after loading the view.
     }
+   
+  
     
+  
 
+   
     @IBAction func buttonComplete(_ sender: Any) {
-        performSegue(withIdentifier: "forgetPsw3ToForgetPswComp", sender: nil)
+        if let code = tbCode.text{
+           
+                if code == verifyCode{
+                    performSegue(withIdentifier: "forgetPsw3ToForgetPswComp", sender: nil)
+                }else{
+                    animationTF()
+                    labelError.isHidden = false
+
+                    
+                
+            }
+           
+        }
     }
     
     
     @IBAction func buttonCancel(_ sender: Any) {
         navigationController?.popToRootViewController(animated: true)
+    }
+    
+    
+    func animationTF(){
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.duration = 0.07
+        animation.repeatCount = 3
+        animation.autoreverses = true
+        animation.fromValue = NSValue(cgPoint: CGPoint(x: tbCode.center.x - 10, y: tbCode.center.y))
+        animation.toValue = NSValue(cgPoint: CGPoint(x: tbCode.center.x + 10, y: tbCode.center.y))
+
+        tbCode.layer.add(animation, forKey: "position")
     }
     
 }
