20a7452ade6301f31f066f9cd183e321c6ddcc5d
hexaque <37326156+hexaque@users.noreply.github.com>
2022-10-18 15:33:30 +0300

    que
    filte sayfası yapılıyor

---
+++ FoodLab_App/AppIsımVer/FilterPageVC.swift
@@ -0,0 +1,63 @@
+//
+//  FilterPageVC.swift
+//  FoodLab_App
+//
+//  Created by Que on 18.10.2022.
+//
+
+import UIKit
+
+class FilterPageVC: UIViewController {
+
+    @IBOutlet weak var sliderLabel: UILabel!
+
+    @IBOutlet weak var TFSiralama: UITextField!
+    var pickerView =  UIPickerView()
+    var siralama = ["Fiyata göre küçükten büyüğe sırala","Fiyata göre büyükten küçüğe sırala"]
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        self.hideKeyboardWhenTappedAround()
+        pickerView.dataSource = self
+        pickerView.delegate = self
+        super.viewDidLoad()
+        TFSiralama.inputView = pickerView
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonCancel(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    @IBAction func sliderDegisim(_ sender: UISlider) {
+        sliderLabel.text = String(Int(sender.value))
+    }
+    @IBAction func buttonFilter(_ sender: Any) {
+    }
+    
+    @IBAction func buttonClear(_ sender: Any) {
+    }
+}
+
+
+extension FilterPageVC : UIPickerViewDelegate,UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return siralama.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return siralama[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        TFSiralama.text = siralama[row]
+        TFSiralama.resignFirstResponder()
+    }
+    
+    
+}
