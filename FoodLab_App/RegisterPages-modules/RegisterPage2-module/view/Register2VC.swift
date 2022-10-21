8b8a3bcc3e36f07d8a7222f6c5ea4c8fad8a23bd
hexaque <37326156+hexaque@users.noreply.github.com>
2022-10-21 14:52:33 +0300

    que
    register2 Güncellendi alertleri validationlar eklendi.

--- FoodLab_App/RegisterPages-modules/RegisterPage2-module/view/Register2VC.swift
+++ FoodLab_App/RegisterPages-modules/RegisterPage2-module/view/Register2VC.swift
@@ -13,15 +13,23 @@ import FirebaseStorage
 
 class Register2VC: UIViewController, UINavigationControllerDelegate,UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var buttonYukle: UIButton!
+    @IBOutlet weak var buttonNext: UIButton!
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var tfPhone: UITextField!
     @IBOutlet weak var tfSurname: UITextField!
     @IBOutlet weak var tfName: UITextField!
     var avatarImageName:String?
+    var isUpload = false
+    var isPhotoSelected = false
     override func viewDidLoad() {
+        indicator.stopAnimating()
         super.viewDidLoad()
         self.hideKeyboardWhenTappedAround()
         imageView.isUserInteractionEnabled = true
+        
+        
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTap))
         imageView.addGestureRecognizer(gestureRecognizer)
     }
@@ -37,50 +45,96 @@ class Register2VC: UIViewController, UINavigationControllerDelegate,UIImagePicke
    
     func imagePickerController(_ picker : UIImagePickerController,didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         imageView.image = info[.originalImage] as? UIImage
+        
+        if imageView.image == info[.originalImage] as? UIImage{
+            isPhotoSelected = true
+        }
+            
+        
         self.dismiss(animated: true)
     }
            
         
     @IBAction func buttonYukle(_ sender: Any) {
-        let store = Storage.storage()
-        let storeRef = store.reference()
-        let uuid = UUID().uuidString
-        let mediaFolder = storeRef.child("images")
-        if let data = imageView.image?.jpegData(compressionQuality: 0.5){
-            let imageRefferance = mediaFolder.child("\(uuid).jpeg")
-            imageRefferance.putData(data, metadata: nil){metadata, error in
-                if error != nil{
-                    print(error?.localizedDescription ?? "error")
-                }else{
-                    imageRefferance.downloadURL{url, error in
-                        if error == nil{
-                            
-                            self.avatarImageName = uuid
-                            
-                            
+        if isPhotoSelected{
+            indicator.startAnimating()
+            self.view.isUserInteractionEnabled = false
+            let store = Storage.storage()
+            let storeRef = store.reference()
+            let uuid = UUID().uuidString
+            let mediaFolder = storeRef.child("images")
+            if let data = imageView.image?.jpegData(compressionQuality: 0.5){
+                let imageRefferance = mediaFolder.child("\(uuid).jpeg")
+                imageRefferance.putData(data, metadata: nil){metadata, error in
+                    if error != nil{
+                        print(error?.localizedDescription ?? "error")
+                    }else{
+                        imageRefferance.downloadURL{url, error in
+                            if error == nil{
+                                
+                                self.avatarImageName = uuid
+                                
+                                self.indicator.stopAnimating()
+                                self.view.isUserInteractionEnabled = true
+                                self.isUpload = true
+                            }
                         }
                     }
+                    
                 }
-                
             }
+            
+        }else{
+            let alertContreller = UIAlertController(title: "Lütfen İlk Olarak Profil Fotoğrafı Seçin ", message:"", preferredStyle: .alert)
+            self.present(alertContreller, animated: true)
+            let okeyAction = UIAlertAction(title: "Tamam", style: .default)
+            alertContreller.addAction(okeyAction)
         }
+        
+    
     }
     
 
     @IBAction func buttonNext(_ sender: Any) {
+   
+        if tfName.text!.count>2 && tfSurname.text!.count > 1 && isValidPhone(phone: tfPhone.text!){
+            if isUpload{
+                let currentUser=Auth.auth().currentUser
+                let user_uid = currentUser?.uid
+               
+                if let uN = tfName.text, let uS = tfSurname.text, let uP = tfPhone.text{
+                    let user = ["user_Uid":user_uid,"user_Name":uN,"user_Surname":uS,"user_Phone":uP,"user_ImageName":avatarImageName! ]
+                    Database.database().reference().child("users").child(user_uid!).setValue(user)
+                   
+                }
+            }
+            else {
+                let alertContreller = UIAlertController(title: "Lütfen Profil Fotoğrafı Yükleyin", message:"", preferredStyle: .alert)
+                self.present(alertContreller, animated: true)
+                let okeyAction = UIAlertAction(title: "Tamam", style: .default)
+                alertContreller.addAction(okeyAction)
+                
+            }
+            
+            }
         
-    
         
-        let currentUser=Auth.auth().currentUser
-        let user_uid = currentUser?.uid
        
-        if let uN = tfName.text, let uS = tfSurname.text, let uP = tfPhone.text{
-            let user = ["user_Uid":user_uid,"user_Name":uN,"user_Surname":uS,"user_Phone":uP,"user_ImageName":avatarImageName! ]
-            Database.database().reference().child("users").child(user_uid!).setValue(user)
-           
+        
+       else{
+            let alertContreller = UIAlertController(title: "Bilgileri girin veya Atla butonuna basın.", message: "Lütfen bilgilerinizi eksiksiz girin. Güvenliğiniz açısından bilgilerinizi doğru girmeniz önemli.", preferredStyle: .alert)
+            self.present(alertContreller, animated: true)
+            let okeyAction = UIAlertAction(title: "Tamam", style: .default)
+            alertContreller.addAction(okeyAction)
         }
+        
+      
        
     }
-    
+    func isValidPhone(phone: String) -> Bool {
+            let phoneRegex = "^[0-9+]{0,1}+[0-9]{5,16}$"
+            let phoneTest = NSPredicate(format: "SELF MATCHES %@", phoneRegex)
+            return phoneTest.evaluate(with: phone)
+        }
 }
 
