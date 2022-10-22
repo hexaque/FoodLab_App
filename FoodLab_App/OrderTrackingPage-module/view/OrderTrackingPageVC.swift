a87d93bd04af4dd957e6101daad77e389315762e
hexaque <37326156+hexaque@users.noreply.github.com>
2022-10-22 17:18:31 +0300

    que
    orderTracking map eklendi izinler eklendi tasarım eklendi düzenlenecek

--- FoodLab_App/OrderTrackingPage-module/view/OrderTrackingPageVC.swift
+++ FoodLab_App/OrderTrackingPage-module/view/OrderTrackingPageVC.swift
@@ -6,24 +6,43 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class OrderTrackingPageVC: UIViewController {
-
+    @IBOutlet weak var map: MKMapView!
+    var locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+        
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+// resto 38.438699,27.1390415
+// home  38.424325, 27.138866
+extension OrderTrackingPageVC: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let sonKonum = locations[locations.count-1]
+        let enlem = sonKonum.coordinate.latitude
+        let boylam = sonKonum.coordinate.longitude
+        let hiz = sonKonum.speed
+       
+        
+        let konum = CLLocationCoordinate2D(latitude: enlem, longitude: boylam)
+        let zoom = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)// ne kadar küçülürse o kadar zoom yapılır
+        let bolge = MKCoordinateRegion(center: konum, span: zoom)
+        map.setRegion(bolge, animated: true)
+        
+        map.showsUserLocation = true
+        
+    }
+}
+
